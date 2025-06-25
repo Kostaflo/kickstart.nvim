4b17dcabdeb4b3180fa32981aa95f1463c313414
KostafloLinux <kostaflo72@gmail.com>
2025-06-25 23:58:43 +0300

    merging conflicts

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,23 @@
 -- See the kickstart.nvim README for more information
 return {
   { 'github/copilot.vim' },
+  {
+    'christoomey/vim-tmux-navigator',
+    vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>'),
+    vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<cr>'),
+    vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<cr>'),
+    vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<cr>'),
+  },
+  {
+    'vim-test/vim-test',
+    dependencies = {
+      'preservim/vimux',
+    },
+    vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', {}),
+    vim.keymap.set('n', '<leader>T', ':TestFile<CR>', {}),
+    vim.keymap.set('n', '<leader>a', ':TestSuite<CR>', {}),
+    vim.keymap.set('n', '<leader>l', ':TestLast<CR>', {}),
+    vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', {}),
+    vim.cmd "let test#strategy = 'vimux'",
+  },
 }
