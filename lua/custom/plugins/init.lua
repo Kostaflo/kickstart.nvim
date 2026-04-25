cdcab55f1e2192cce57467d0f528cf04e700d358
KostafloLinux <kostaflo72@gmail.com>
2026-04-25 18:11:30 +0300

    Added keybinds to copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,7 +7,18 @@
 ---@type LazySpec
 return {
 
-  { 'github/copilot.vim' },
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)')
+      vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)')
+    end,
+  },
   {
     'christoomey/vim-tmux-navigator',
     vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>'),
